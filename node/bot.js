71cfb4a20e8b5aa27b43ce6daeb5c56748b50962
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2017-10-06 02:01:23 -0400

    Initial node.js setup

---
+++ node/bot.js
@@ -0,0 +1,23 @@
+
+var Botkit = require('botkit');
+//var childProcess = require("child_process");
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: process.env.SLACKTOKEN,
+}).startRTM()
+
+// give the bot something to listen for.
+//controller.hears('string or regex',['direct_message','direct_mention','mention'],function(bot,message) {
+controller.hears('[\s\S]*',['mention', 'direct_mention','direct_message'], function(bot,message) 
+{
+  console.log(message);
+  bot.reply(message, message.text);
+});
+
