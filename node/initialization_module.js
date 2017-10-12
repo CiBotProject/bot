f2ade4df110b1f337cd348fad9fa48af3f62c09b
Timothy-Dement <tmdement@ncsu.edu>
2017-10-12 15:50:22 -0400

    Add require statements and comments for methods.

---
+++ node/initialization_module.js
@@ -0,0 +1,25 @@
+// I am almost certain that I need these:
+var request = require('request');
+var Promise = require('bluebird');
+
+// I have no idea if I need these:
+var _ = require('underscore');
+var fs = require('fs');
+var parse = require('parse-link-header');
+var querystring = require('querystring');
+
+// For open source projects tested on travis-ci.org, use https://api.travis-ci.org.
+
+// Define method to check for Travis CI file.
+
+// Define method to create Travis CI file.
+
+// Define method to update Travis CI file.
+
+// Define method to check for Coveralls file.
+
+// Define method to create Coveralls file.
+
+// Define method to update Coveralls file.
+
+// Export methods for external use.
