5a814c2bdc2a0f5115b30cb87983c1f6233b1bf2
Sourabh Saha <sourabhsaha@Sourabhs-MacBook-Air.local>
2017-10-12 09:59:24 -0400

    converstion work started

--- node/bot.js
+++ node/bot.js
@@ -15,17 +15,18 @@ controller.spawn({
 
 // give the bot something to listen for.
 //controller.hears('string or regex',['direct_message','direct_mention','mention'],function(bot,message) {
-controller.hears('[\s\S]*',['mention', 'direct_mention','direct_message'], function(bot,message) 
+/*controller.hears('[\s\S]*',['mention', 'direct_mention','direct_message'], function(bot,message)
 {
   console.log(message);
   bot.reply(message, message.text);
 });
+*/
 
 /**
  * Selenium testing for presence of bot
  * TODO: pull out all selenium tests into their own module
  */
-controller.hears('hello world, from Selenium',['mention', 'direct_mention','direct_message', 'ambient'], function(bot,message) 
+controller.hears('hello world, from Selenium',['mention', 'direct_mention','direct_message', 'ambient'], function(bot,message)
 {
   console.log(message);
   bot.reply(message, message.text);
