06c8f0bcba34c000ef36ee4cd4d68e3224ad861f
samuel <sgonsal@ncsu.edu>
2017-10-23 14:37:57 -0400

    Mocking coverall service

--- node/modules/coveralls.js
+++ node/modules/coveralls.js
@@ -1,10 +1,32 @@
-var Promise = require("bluebird");
+ var Promise = require("bluebird");
 var chai = require("chai");
 var expect = chai.expect;
 var request = require("request");
 var nock = require("nock");
 var _ = require("underscore");
 
+
+var data = require("../modules/mocks/coverallsMock.json");
+
+function getCoverageInfo(commitSHA)
+{
+	var mockCoverallsService = nock("https://coveralls.io")
+			.get("/builds/" + commitSHA + ".json")
+			.reply(200, JSON.stringify(data));
+
+	var coverageInfo = {
+		"committer_name": data.committer_name,
+		"created_at": data.created_at,
+		"covered_percent": data.covered_percent,
+		"coverage_change": data.coverage_change 	
+	};
+	
+	return(coverageInfo);
+}
+
+/*
+
+Actual SERVICE code:
 
 function getCoverageInfo(commitSHA)
 {
@@ -19,6 +41,8 @@ function getCoverageInfo(commitSHA)
 		});
 	});
 }
+
+*/
 
 /*
 
