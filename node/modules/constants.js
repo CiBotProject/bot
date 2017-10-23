a84d69c8e5825516027510096f7c9147c935d4ea
arewm <ajmcnama@ncsu.edu>
2017-10-23 13:07:24 -0400

    comment constants

--- node/modules/constants.js
+++ node/modules/constants.js
@@ -1,4 +1,7 @@
 
 exports.constants.FAILURE = 'failure'
 exports.constants.SUCCESS = 'success'
-exports.constants.message = {'status': '', 'message': '', 'body': '', 'blame': []}
+exports.constants.message = {'status': '',  // Status, either FAILURE or SUCCESS as above
+                             'message': '', // Message to send back user
+                             'body': '',    // Body of message to next module, if necessary (i.e. issue body)
+                             'blame': []}   // Users to blame for an issue if created
