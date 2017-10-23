4874681076a27a293d7dce43b1a75175860ef817
arewm <ajmcnama@ncsu.edu>
2017-10-23 15:33:07 -0400

    Revert "change interal message format"
    This reverts commit 1c49451b146e308653d24368a65753a7663a0c16.

--- node/modules/constants.js
+++ node/modules/constants.js
@@ -1,10 +1,7 @@
-// Data object to pass to the next caller.
-// If you expect this data to come from another function, add the field with a default below
-exports.data = {'body': '',
-                'blame': []}
 
 exports.constants.FAILURE = 'failure'
 exports.constants.SUCCESS = 'success'
 exports.constants.message = {'status': '',  // Status, either FAILURE or SUCCESS as above
                              'message': '', // Message to send back user
-                             'data': data}  // Data to pass to the next caller
+                             'body': '',    // Body of message to next module, if necessary (i.e. issue body)
+                             'blame': []}   // Users to blame for an issue if created
