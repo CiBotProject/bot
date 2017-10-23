1c49451b146e308653d24368a65753a7663a0c16
arewm <ajmcnama@ncsu.edu>
2017-10-23 15:31:23 -0400

    change interal message format

--- node/modules/constants.js
+++ node/modules/constants.js
@@ -1,7 +1,10 @@
+// Data object to pass to the next caller.
+// If you expect this data to come from another function, add the field with a default below
+exports.data = {'body': '',
+                'blame': []}
 
 exports.constants.FAILURE = 'failure'
 exports.constants.SUCCESS = 'success'
 exports.constants.message = {'status': '',  // Status, either FAILURE or SUCCESS as above
                              'message': '', // Message to send back user
-                             'body': '',    // Body of message to next module, if necessary (i.e. issue body)
-                             'blame': []}   // Users to blame for an issue if created
+                             'data': data}  // Data to pass to the next caller
