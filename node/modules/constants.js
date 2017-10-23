03f7f0fc54dc415e53357a9b32fbb6dbc4c1badc
arewm <ajmcnama@ncsu.edu>
2017-10-23 16:42:12 -0400

    fix data export

--- node/modules/constants.js
+++ node/modules/constants.js
@@ -1,8 +1,8 @@
 // Data object to pass to the next caller.
 // If you expect this data to come from another function, add the field with a default below
-exports.data = {'body': '',
-                'blame': []
-            }
+var data = {'body': '',
+            'blame': []
+        }
 
 exports.FAILURE = 'failure'
 exports.SUCCESS = 'success'
@@ -10,3 +10,5 @@ exports.message = {'status': '',    // Status, either FAILURE or SUCCESS as abov
                   'message': '',    // Message to send back user
                   'data': data     // Data to pass to the next caller
                 }
+
+exports.data = data;
