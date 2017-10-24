fd8120a44a7afe37266457440bc0372b780dc4ac
Sourabh Saha <sourabhsaha@Sourabhs-MacBook-Air.local>
2017-10-23 23:23:57 -0400

    communication added for issue creation, travis integration, coveralls integration

--- node/bot.js
+++ node/bot.js
@@ -10,7 +10,7 @@ var controller = Botkit.slackbot({
   //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
-var repoName = null;
+var tempIssueName = "";
 
 var globals = {
   coverageMap:{},//channel:threshold amount
@@ -29,6 +29,7 @@ controller.hears(['init travis'],['direct_message','direct_mention','mention'],f
   var messageArray = message.text.split(' ');
   var index = messageArray.indexOf('travis');
 
+
   if(messageArray.indexOf('help')===-1){
     //repo name has to be word after init
     if((index+1)<messageArray.length)
@@ -36,61 +37,19 @@ controller.hears(['init travis'],['direct_message','direct_mention','mention'],f
     //if repo name is provided
     if(repoString!==null){
       //format is owner/repo-name
-      bot.startConversation(message,function(err,convo){
-        convo.say("I am initializing the repository...");
-        //add entry of channel_name:repo_name to the repoMap
-        var repoContent = repoString.split('/');
+      var repoContent = repoString.split('/');
 
-        globals.repoMap[message.channel]=repoContent[1];
-        globals.ownerMap[message.channel]=repoContent[0];
-        //create default coverageMap entry
-        globals.coverageMap[message.channel]=globals.defaultThreshold;
+      globals.repoMap[message.channel]=repoContent[1];
+      globals.ownerMap[message.channel]=repoContent[0];
+      //create default coverageMap entry
+      globals.coverageMap[message.channel]=globals.defaultThreshold;
 
-        Travis.activate(repoContent[0],repoContent[1],function(data){
-          bot.reply(message,data.message);
-        });
-        /*
-        convo.ask('Would you like to create a yaml file as well?',[
-          {
-            pattern:bot.utterances.yes,
-            callback:function(response,convo){
-              convo.next();
-              convo.ask('Which language do you want to use? '+Travis.listTechnologies().data.body.join(','),function(response,convo){
-                //TODO:support for random string with tech name
-                var yamlStatus = Travis.createYaml(response.text);
-                if(yamlStatus.status==='success'){
-                    //yamlStatus.data.body needs to be passed
-                    convo.say("Yaml created");
-                    //convo.next();
-                }
-                else{
-                    convo.say("Error in creating yaml file");
-                    //convo.next();
-                }
-              });
-            }
-          },
-          {
-            pattern:bot.utterances.no,
-            callback:function(response,convo){
-              convo.next();
-              convo.say("Alright, continuing with initialization");
-              //github init
-              //convo.next();
-            }
-          },
-          {
-            default:true,
-            callback:function(response,convo){
-              convo.next();
-              convo.repeat();
-              //convo.next();
-            }
-          }
-        ],{},'default');
-        */
-        ////convo.next();
+      Travis.activate(repoContent[0],repoContent[1],function(data){
+        bot.reply(message,data.message);
       });
+
+      bot.startConversation(message,askYamlCreation);
+
     }
     else{
       bot.reply(message,"Please provide the name of the repository to be initialized. Ex init <repository>");
@@ -100,39 +59,65 @@ controller.hears(['init travis'],['direct_message','direct_mention','mention'],f
     bot.reply(message,helpCommands().init);
   }
 });
+//helper functions
+askYamlCreation = function(response,convo){
+  convo.ask('Would you like to create a yaml file (yes/no)?',function(response,convo){
+    if(response.text.toLowerCase()==="yes"){
+      askLanguageToUse(response,convo);
+
+    }else{
+      convo.say("Initializing repository without yaml");
+    }
+    convo.next();
+  });
+}
+
+askLanguageToUse = function(response,convo){
+  convo.ask('Which language do you want to use ? '+Travis.listTechnologies().data.body.join(','),function(response,convo){
+    var yamlStatus = Travis.createYaml(response.text);
+    if(yamlStatus.status==='success'){
+        //yamlStatus.data.body needs to be passed
+        convo.say("Yaml created");
+    }
+    else{
+        convo.say("Error in creating yaml file");
+    }
+    convo.next();
+  });
+}
+
 //configure yaml
-controller.hears(['configure'],['direct_message','direct_mention','mention'],function(bot,message){
+controller.hears(['configure yaml'],['direct_message','direct_mention','mention'],function(bot,message){
   //start conversation with one user, replies from other users should not affect this conversation
   //TODO: test this functionality by letting a different user reply, expected outcome should be no reply from bot to that user
   var messageArray = message.text.split(' ');
-  var index = messageArray.indexOf('configure');
+  var index = messageArray.indexOf('yaml');
+
+
   if(messageArray.indexOf('help')===-1){
     //repo name has to be word after init
     if((index+1)<messageArray.length)
-        repoName = messageArray[index+1];
+        repoString = messageArray[index+1];
+    //if repo name is provided
+    if(repoString!==null){
+      //format is owner/repo-name
+      var repoContent = repoString.split('/');
 
-    if(repoName!=null)
-      bot.startConversation(message,function(err,convo){
-        convo.ask('Which language do you want to use? ( Javascript with NodeJS, Ruby or Python)',function(response,convo){
+      globals.repoMap[message.channel]=repoContent[1];
+      globals.ownerMap[message.channel]=repoContent[0];
+      //create default coverageMap entry
+      globals.coverageMap[message.channel]=globals.defaultThreshold;
 
-          if(response.text.toLowerCase().includes("node")){
-            //TODO:store environment in the database
-            convo.say("Great, give me a minute to setup the configuration file! I'll let you know when it's done..");
-            //convo.next();
-            initializeRepository(bot,message,repoName,"node");
+      bot.startConversation(message,askLanguageToUse);
 
-          }
-          else{
-            convo.say("Sorry, I am not smart enough to create that config :disappointed:");
-            //convo.next();
-          }
-        });
-
-      });
     }
     else{
-      bot.reply(message,helpCommands().configure);
+      bot.reply(message,"Please provide the name of the repository to be configured. Ex configure yaml <owner>/<repository>");
     }
+  }
+  else{
+    bot.reply(message,helpCommands().configure);
+  }
 });
 //test last build and create issue on failure
 controller.hears(['test last build'],['direct_message','direct_mention','mention'],function(bot,message){
@@ -140,14 +125,16 @@ controller.hears(['test last build'],['direct_message','direct_mention','mention
   Travis.lastBuild(globals.ownerMap[message.channel],globals.repoMap[message.channel],function(data){
     bot.reply(message,data.message);
     if(data.status==='failure')
-      issueCreationConversation(bot,message,`Build failure`);
+      issueCreationConversation(bot,message,`Build failure`,"");
   });
 
 });
+
 //testing issue creation
 controller.hears(['test issue'],['direct_message','direct_mention','mention'],function(bot,message){
   issueCreationConversation(bot,message);
 });
+
 //testing Travis
 controller.hears(['test travis','test Travis'],['direct_message','direct_mention','mention'],function(bot,message){
   bot.reply(message,"Testing travis");
@@ -211,7 +198,7 @@ controller.hears(['help'],['direct_message','direct_mention','mention'],function
 function helpCommands(){
   return{
     init:"*_init travis <owner>/<repo_name>_*",
-    configure:"*_configure <owner>/<repository>_*",
+    configure:"*_configure yaml <owner>/<repository>_*",
     issue:"*_test issue_*",
     travis:"*_test travis_*",
     coveralls:"*_test coveralls_*"
@@ -235,64 +222,53 @@ function initializeRepository(bot,message,repoName,framework){
 }
 
 function issueCreationConversation(bot,message,issueTitle){
-
-  var issueFlag = true,issueChange = false;
-
-  bot.startConversation(message,function(err,convo){
-
-    convo.addQuestion('Do you want to create an issue (yes/no)?',function(response,convo){
-      if(response.text.toLowerCase().includes("yes")){
-        issueFlag = true;
+  tempIssueName = issueTitle;
+  bot.startConversation(message,askToCreateIssue);
+}
+//helper functions
+askToCreateIssue = function(response,convo){
+  convo.ask('Do you want to create an issue (yes/no)?',function(response,convo){
+    if(response.text.toLowerCase()==="yes"){
+      if(tempIssueName===""){
+        askToCreateNewIssue(response,convo);
       }
       else{
-        issueFlag = false;
+        askToCreateExistingIssue(response,convo);
       }
-      convo.next();
-    },{},'default');
-
-
-
-    if(issueFlag){
-      //if no issue title exists
-      if(!issueTitle || issueTitle===""){
-          convo.addQuestion('Please enter the title of the issue',function(response,convo){
-            convo.say("Thanks! I'll name the issue *"+response.text+"*");
-            issueTitle = response.text;
-            convo.next();
-          },{},'default');
-      }
-      else{
-          convo.addQuestion('Current issue title is set to *_'+issueTitle+'_*.Do you want to change the title of the issue (yes/no)?',function(response,convo){
-            if(response.text.toLowerCase().includes("yes")){
-              issueChange = true;
-            }else{
-              issueChange = false;
-            }
-            convo.next();
-          },{},'default');
-
-      }
-
-      if(issueChange){
-          convo.addQuestion('Please enter the title of the issue',function(response,convo){
-            convo.say("Thanks! I'll name the issue *"+response.text+"*");
-            issueTitle = response.text;
-            convo.next();
-          },{},'default');
-      }else{
-        bot.reply(message,"No problem, I'll keep the title as it is!");
-      }
-
-        convo.addQuestion('Please enter a comma-separated list of assignees to the issue. Ex @user1,@user2,@user3...',function(response,convo){
-          var assigneeList = response.text.split(',');
-          var listOutput = response.text;
-          convo.say(`I am going to create an issue titled *${issueTitle}* and assign it to `+listOutput);
-          convo.next();
-        },{},'default');
-
     }
     else{
-      bot.reply(message,'Okay, I\'m not creating an issue');
+      convo.say("I'll not create the issue");
     }
+    convo.next();
+  });
+}
+
+askToCreateNewIssue = function(response,convo){
+  convo.ask('Please enter the name of the issue',function(response,convo){
+    tempIssueName = response.text;
+    convo.say("I'm creating an issue titled *"+tempIssueName+"*");
+    askToAssignPeople(response,convo);
+    convo.next();
+  });
+}
+
+askToCreateExistingIssue = function(response,convo){
+  convo.ask('Current issue title is set to *'+tempIssueName+'*.Do you want to change the title of the issue (yes/no)',function(response,convo){
+    if(response.text.toLowerCase()==="yes"){
+      askToCreateNewIssue(response,convo);
+    }
+    else{
+      askToAssignPeople(response,convo);
+    }
+    convo.next();
+  });
+}
+
+askToAssignPeople = function(response,convo){
+  convo.ask('Please enter a comma-separated list of assignees to the issue. Ex @user1,@user2,@user3...',function(response,convo){
+    var listOutput = response.text;
+    convo.say("I am going to create an issue titled *"+tempIssueName+"* and assign it to "+listOutput);
+    tempIssueName = "";
+    convo.next();
   });
 }
