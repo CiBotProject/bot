3737e56c5cfaad8371cb887816689be679b6d8f6
arewm <ajmcnama@ncsu.edu>
2017-10-23 21:44:05 -0400

    make things easier by making helper function

--- Selenium/src/test/java/selenium/tests/WebTest.java
+++ Selenium/src/test/java/selenium/tests/WebTest.java
@@ -30,6 +30,7 @@ public class WebTest
 	private static WebDriver driver;
 	private static WebDriverWait wait;
 	private static String botName = System.getenv("SLACK_BOT_NAME");
+//	private static int numResponses = 0;
 	
 	@BeforeClass
 	public static void setUp() throws Exception 
@@ -79,7 +80,8 @@ public class WebTest
 	 * @param minCount
 	 * @return
 	 */
-	public List<WebElement> waitUntilCountChanges(final String xpath, final int minCount) {
+	private List<WebElement> waitUntilCountChanges(final String xpath, final int minCount)
+	{
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver driver) {
@@ -94,23 +96,10 @@ public class WebTest
         
     }
 	
-	/**
-	 * 
-	 */
-	@Test
-	public void helpMessage()
+	private void testCommandOneResponse(String command, String expectedResponse)
 	{
-		/*
-		 * 	BOILERPLATE
-		 */
-		
-		/*
-		 *	TESTS
-		 */
-		
 		String xpathSearch = "//div[@class='message_content_header_left']/a[.= '" + botName + "']";
 		String messageBodyRel = "../../following-sibling::span[@class='message_body']";
-		
 		// Type in the help command 
 		WebElement messageBot = driver.findElement(By.id("msg_input"));
 		assertNotNull(messageBot);
@@ -123,9 +112,9 @@ public class WebTest
 		Actions actions = new Actions(driver);
 		actions.moveToElement(messageBot);
 		actions.click();
+		actions.click();		// not sure if this does anything, but keystrokes were not registering
 		actions.click();
-		actions.click();
-		actions.sendKeys("@" + botName + " help");
+		actions.sendKeys(command);
 		actions.sendKeys(Keys.RETURN);
 		actions.build().perform();
 
@@ -140,147 +129,67 @@ public class WebTest
 	
 		// Make sure that we have the right text
 		assertNotNull(lastBody);
-		assertEquals("help init or help configure or help issue or help travis or help coveralls", 
-				lastBody.getText());
+		assertEquals(expectedResponse, lastBody.getText());
+	}
+	
+	private void testCommandTwoResponses(String command, String expectedResponse1, String expectedResponse2)
+	{
+		String xpathSearch = "//div[@class='message_content_header_left']/a[.= '" + botName + "']";
+		String messageBodyRel = "../../following-sibling::span[@class='message_body']";
+		// Type in the help command 
+		WebElement messageBot = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBot);
+		int numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
 		
 		/*
-		 *  HELP INIT
+		 * DEFAULT HELP
 		 */
+		
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBot);
+		actions.click();
+		actions.click();		// not sure if this does anything, but keystrokes were not registering
+		actions.click();
+		actions.sendKeys(command);
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
 
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsInit = new Actions(driver);
-		actionsInit.moveToElement(messageBot);
-		actionsInit.click();
-		actionsInit.click();
-		actionsInit.click();
-		actionsInit.sendKeys("@" + botName + " help init");
-		actionsInit.sendKeys(Keys.RETURN);
-		actionsInit.build().perform();
-		
 		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
+		List<WebElement> messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 2);
 		// Make sure that we have a new messages
 		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("init <repository>", 
-				lastBody.getText());
-		
-		/*
-		 *  HELP CONFIGURE
-		 */
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsConfigure = new Actions(driver);
-		actionsConfigure.moveToElement(messageBot);
-		actionsConfigure.click();
-		actionsConfigure.click();
-		actionsConfigure.click();
-		actionsConfigure.sendKeys("@" + botName + " help configure");
-		actionsConfigure.sendKeys(Keys.RETURN);
-		actionsConfigure.build().perform();
-		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("configure <repository>", 
-				lastBody.getText());
-		
-		/*
-		 *  HELP ISSUE
-		 */
+		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 2);
 
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsIssue = new Actions(driver);
-		actionsIssue.moveToElement(messageBot);
-		actionsIssue.click();
-		actionsIssue.click();
-		actionsIssue.click();
-		actionsIssue.sendKeys("@" + botName + " help issue");
-		actionsIssue.sendKeys(Keys.RETURN);
-		actionsIssue.build().perform();
-		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
+		WebElement secondLastElement = messages.get(messages.size() -2);
+		WebElement secondLastBody = secondLastElement.findElement(By.xpath(messageBodyRel));
+		WebElement lastElement = messages.get(messages.size() - 1);
+		WebElement lastBody = lastElement.findElement(By.xpath(messageBodyRel));
 	
 		// Make sure that we have the right text
+		assertNotNull(secondLastBody);
+		assertEquals(expectedResponse1, secondLastBody.getText());
 		assertNotNull(lastBody);
-		assertEquals("test issue", 
-				lastBody.getText());
-		
-		/*
-		 *  HELP TRAVIS
-		 */
-
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsTravis = new Actions(driver);
-		actionsTravis.moveToElement(messageBot);
-		actionsTravis.click();
-		actionsTravis.click();
-		actionsTravis.click();
-		actionsTravis.sendKeys("@" + botName + " help travis");
-		actionsTravis.sendKeys(Keys.RETURN);
-		actionsTravis.build().perform();
-		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
+		assertEquals(expectedResponse2, lastBody.getText());
+	}
 	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("test travis", 
-				lastBody.getText());
+	/**
+	 * 
+	 */
+	@Test
+	public void helpMessage()
+	{
+		testCommandOneResponse("@" + botName + " help", 
+				"help init or help configure or help issue or help travis or help coveralls");
 		
-		/*
-		 *  HELP COVERALLS
-		 */
-
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsCoveralls = new Actions(driver);
-		actionsCoveralls.moveToElement(messageBot);
-		actionsCoveralls.click();
-		actionsCoveralls.click();
-		actionsCoveralls.click();
-		actionsCoveralls.sendKeys("@" + botName + " help coveralls");
-		actionsCoveralls.sendKeys(Keys.RETURN);
-		actionsCoveralls.build().perform();
+		testCommandOneResponse("@" + botName + " help init", "init <repository>");
 		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
+		testCommandOneResponse("@" + botName + " help configure", "configure <repository>");
 		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("test coveralls", 
-				lastBody.getText());
+		testCommandOneResponse("@" + botName + " help issue", "test issue");
+		
+		testCommandOneResponse("@" + botName + " help travis", "test travis");
+		
+		testCommandOneResponse("@" + botName + " help coveralls", "test coveralls");
 	}
 	
 	/**
@@ -336,230 +245,52 @@ public class WebTest
 	 */
 	@Test
 	public void useCase3()
-	{
-
-		/*
-		 * 	BOILERPLATE
-		 */
-		
-		/*
-		 *	TESTS
-		 */
-		
-		String xpathSearch = "//div[@class='message_content_header_left']/a[.= '" + botName + "']";
-		String messageBodyRel = "../../following-sibling::span[@class='message_body']";
-		
-		// Type in the help command 
-		WebElement messageBot = driver.findElement(By.id("msg_input"));
-		assertNotNull(messageBot);
-		int numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		
+	{		
 		/*
 		 *  SETTING THE THRESHOLD - LOW
 		 */
-		
-		Actions actionsThreshold = new Actions(driver);
-		actionsThreshold.moveToElement(messageBot);
-		actionsThreshold.click();
-		actionsThreshold.click();
-		actionsThreshold.click();
-		actionsThreshold.sendKeys("@" + botName + " set coverage threshold to 5");
-		actionsThreshold.sendKeys(Keys.RETURN);
-		actionsThreshold.build().perform();
-
-		// Execute the actions and wait until the number of messages changes
-		List<WebElement> messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		WebElement lastElement = messages.get(messages.size() - 1);
-		WebElement lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("The coverage threshold has been set to 5", 
-				lastBody.getText());
+		testCommandOneResponse("@" + botName + " set coverage threshold to 5", 
+				"The coverage threshold has been set to 5");
 		
 		/*
 		 *  COVERAGE IS GOOD
 		 */
-
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsCoverageGood = new Actions(driver);
-		actionsCoverageGood.moveToElement(messageBot);
-		actionsCoverageGood.click();
-		actionsCoverageGood.click();
-		actionsCoverageGood.click();
-		actionsCoverageGood.sendKeys("@" + botName + " test coveralls");
-		actionsCoverageGood.sendKeys(Keys.RETURN);
-		actionsCoverageGood.build().perform();
-		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("Current coverage is (91%)", 
-				lastBody.getText());
+		testCommandOneResponse("@" + botName + " test coveralls", "Current coverage is (91%)");
 		
 		/*
 		 *  SETTING THE THRESHOLD - HIGH
 		 */
-
-		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-		Actions actionsThresholdHigh = new Actions(driver);
-		actionsThresholdHigh.moveToElement(messageBot);
-		actionsThresholdHigh.click();
-		actionsThresholdHigh.click();
-		actionsThresholdHigh.click();
-		actionsThresholdHigh.sendKeys("@" + botName + " set coverage threshold to 95");
-		actionsThresholdHigh.sendKeys(Keys.RETURN);
-		actionsThresholdHigh.build().perform();
+		testCommandOneResponse("@" + botName + " set coverage threshold to 95", 
+				"The coverage threshold has been set to 95");
 		
-		// Execute the actions and wait until the number of messages changes
-		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-		// Make sure that we have a new messages
-		assertTrue("There were no messages", messages.size() > 0);
-		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
+		/*
+		 *  COVERAGE IS BAD
+		 */
+		testCommandTwoResponses("@" + botName + " test coveralls", 
+				"Current coverage (91%) is below threshold (95%)", 
+				"Current issue title is set to Coverage 4 percent below threshold."
+				+ "Do you want to change the title of the issue (yes/no)?");
 		
-		lastElement = messages.get(messages.size() - 1);
-		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-	
-		// Make sure that we have the right text
-		assertNotNull(lastBody);
-		assertEquals("The coverage threshold has been set to 95", 
-				lastBody.getText());
+		/*
+		 * 	NOT CREATING THE ISSUE
+		 */
+//		testCommandOneResponse("@" + botName + " help issue", "test issue");
 		
-//		/*
-//		 *  COVERAGE IS BAD
-//		 */
-//
-//		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-//		Actions actionsCoverageBad = new Actions(driver);
-//		actionsCoverageBad.moveToElement(messageBot);
-//		actionsCoverageBad.click();
-//		actionsCoverageBad.click();
-//		actionsCoverageBad.click();
-//		actionsCoverageBad.sendKeys("@" + botName + " test coveralls");
-//		actionsCoverageBad.sendKeys(Keys.RETURN);
-//		actionsCoverageBad.build().perform();
-//		
-//		// Execute the actions and wait until the number of messages changes
-//		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 2);
-//		// Make sure that we have a new messages
-//		assertTrue("There were no messages", messages.size() > 0);
-//		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 2);
-//
-//		WebElement secondLastElement = messages.get(messages.size() -2);
-//		WebElement secondLastBody = secondLastElement.findElement(By.xpath(messageBodyRel));
-//		lastElement = messages.get(messages.size() - 1);
-//		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-//	
-//		// Make sure that we have the right text
-//		assertNotNull(secondLastBody);
-//		assertEquals("Current coverage (91%) is below threshold (95%)", 
-//				secondLastBody.getText());
-//		assertNotNull(lastBody);
-//		assertEquals("Current issue title is set to Coverage 4 percent below threshold."
-//				+ "Do you want to change the title of the issue (yes/no)?", 
-//				lastBody.getText());
-//		
-//		/*
-//		 * 	NOT CREATING THE ISSUE
-//		 */
-//		
-//		// TODO
-//		
-//		/*
-//		 *  CHANGE ISSUE TITLE
-//		 */
-//
-////		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-////		Actions actionsCoveralls = new Actions(driver);
-////		actionsCoveralls.moveToElement(messageBot);
-////		actionsCoveralls.click();
-////		actionsCoveralls.click();
-////		actionsCoveralls.click();
-////		actionsCoveralls.sendKeys("yes");
-////		actionsCoveralls.sendKeys(Keys.RETURN);
-////		actionsCoveralls.build().perform();
-////		
-////		// Execute the actions and wait until the number of messages changes
-////		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-////		// Make sure that we have a new messages
-////		assertTrue("There were no messages", messages.size() > 0);
-////		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-////		
-////		lastElement = messages.get(messages.size() - 1);
-////		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-////	
-////		// Make sure that we have the right text
-////		assertNotNull(lastBody);
-////		assertEquals("", 
-////				lastBody.getText());
-//		
-//		/*
-//		 *  ADD ISSUE ASSIGNEES
-//		 */
-//
-//		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-//		Actions actionsAddAssignees = new Actions(driver);
-//		actionsAddAssignees.moveToElement(messageBot);
-//		actionsAddAssignees.click();
-//		actionsAddAssignees.click();
-//		actionsAddAssignees.click();
-//		actionsAddAssignees.sendKeys("@arewm");
-//		actionsAddAssignees.sendKeys(Keys.RETURN);
-//		actionsAddAssignees.build().perform();
-//		
-//		// Execute the actions and wait until the number of messages changes
-//		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-//		// Make sure that we have a new messages
-//		assertTrue("There were no messages", messages.size() > 0);
-//		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-//		
-//		lastElement = messages.get(messages.size() - 1);
-//		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-//	
-//		// Make sure that we have the right text
-//		assertNotNull(lastBody);
-//		assertEquals("I am going to create an issue titled Coverage 4 percent below threshold and assign it to arewm", 
-//				lastBody.getText());
-//		
-//		/*
-//		 *  NOT ADD ISSUE ASSIGNEES
-//		 */
-//
-////		numMessagesBefore = driver.findElements(By.xpath(xpathSearch)).size();
-////		Actions actionsNotAddAssignees = new Actions(driver);
-////		actionsNotAddAssignees.moveToElement(messageBot);
-////		actionsNotAddAssignees.click();
-////		actionsNotAddAssignees.click();
-////		actionsNotAddAssignees.click();
-////		actionsNotAddAssignees.sendKeys("yes");
-////		actionsNotAddAssignees.sendKeys(Keys.RETURN);
-////		actionsNotAddAssignees.build().perform();
-////		
-////		// Execute the actions and wait until the number of messages changes
-////		messages = waitUntilCountChanges(xpathSearch, numMessagesBefore + 1);
-////		// Make sure that we have a new messages
-////		assertTrue("There were no messages", messages.size() > 0);
-////		assertTrue("No new messages were found", messages.size() == numMessagesBefore + 1);
-////		
-////		lastElement = messages.get(messages.size() - 1);
-////		lastBody = lastElement.findElement(By.xpath(messageBodyRel));
-////	
-////		// Make sure that we have the right text
-////		assertNotNull(lastBody);
-////		assertEquals("", 
-////				lastBody.getText());
+		// TODO
+		
+		/*
+		 *  CHANGE ISSUE TITLE
+		 */
+//		testCommandOneResponse("", "");
+		
+		/*
+		 *  ADD ISSUE ASSIGNEES
+		 */
+		testCommandOneResponse("@arewm", "I am going to create an issue titled Coverage 4 percent below threshold and assign it to arewm");
+		
+		/*
+		 *  NOT ADD ISSUE ASSIGNEES
+		 */
+//		testCommandOneResponse("", "");
 	}
-
 }
