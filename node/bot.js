fb12f892b5348ffb1b0b723ce5ece32599c7dac5
Sourabh Saha <sourabhsaha@Sourabhs-MacBook-Air.local>
2017-10-23 22:29:45 -0400

    testing communication

--- node/bot.js
+++ node/bot.js
@@ -1,6 +1,7 @@
 
 var Botkit = require('botkit');
 var Coveralls = require('./modules/coveralls');
+var Travis = require('./modules/travis');
 //var childProcess = require("child_process");
 
 var controller = Botkit.slackbot({
@@ -14,6 +15,7 @@ var repoName = null;
 var globals = {
   coverageMap:{},//channel:threshold amount
   repoMap:{},//channel:repo name
+  ownerMap:{},
   defaultThreshold:90,
 };
 
@@ -23,39 +25,47 @@ controller.spawn({
 }).startRTM()
 
 //init repository
-controller.hears(['init'],['direct_message','direct_mention','mention'],function(bot,message){
+controller.hears(['init travis'],['direct_message','direct_mention','mention'],function(bot,message){
   var messageArray = message.text.split(' ');
-  var index = messageArray.indexOf('init');
+  var index = messageArray.indexOf('travis');
 
   if(messageArray.indexOf('help')===-1){
     //repo name has to be word after init
     if((index+1)<messageArray.length)
-        repoName = messageArray[index+1];
+        repoString = messageArray[index+1];
     //if repo name is provided
-    if(repoName!==null){
+    if(repoString!==null){
+      //format is owner/repo-name
       bot.startConversation(message,function(err,convo){
         convo.say("I am initializing the repository...");
         //add entry of channel_name:repo_name to the repoMap
-        globals.repoMap[message.channel]=repoName;
+        var repoContent = repoString.split('/');
+
+        globals.repoMap[message.channel]=repoContent[1];
+        globals.ownerMap[message.channel]=repoContent[0];
         //create default coverageMap entry
         globals.coverageMap[message.channel]=globals.defaultThreshold;
 
+        Travis.activate(repoContent[0],repoContent[1],function(data){
+          bot.reply(message,data.message);
+        });
+        /*
         convo.ask('Would you like to create a yaml file as well?',[
           {
             pattern:bot.utterances.yes,
             callback:function(response,convo){
               convo.next();
-              convo.ask('Which language do you want to use? ( Javascript with NodeJS, Ruby or Python)',function(response,convo){
-                if(response.text.toLowerCase().includes("node")){
-                  //TODO:store environment in the database
-                  convo.say("Great, give me a minute to setup the configuration! I'll let you know when it's done..");
-                  convo.next();
-                  //to simulate the delay in response for creating config file
-                  initializeRepository(bot,message,repoName,"node");
+              convo.ask('Which language do you want to use? '+Travis.listTechnologies().data.body.join(','),function(response,convo){
+                //TODO:support for random string with tech name
+                var yamlStatus = Travis.createYaml(response.text);
+                if(yamlStatus.status==='success'){
+                    //yamlStatus.data.body needs to be passed
+                    convo.say("Yaml created");
+                    //convo.next();
                 }
                 else{
-                  convo.say("Sorry, I am not smart enough to create that config :disappointed:");
-                  convo.next();
+                    convo.say("Error in creating yaml file");
+                    //convo.next();
                 }
               });
             }
@@ -63,19 +73,23 @@ controller.hears(['init'],['direct_message','direct_mention','mention'],function
           {
             pattern:bot.utterances.no,
             callback:function(response,convo){
-              convo.say("Alright, continuing with initialization");
-              initializeRepository(bot,message,repoName);
               convo.next();
+              convo.say("Alright, continuing with initialization");
+              //github init
+              //convo.next();
             }
           },
           {
             default:true,
             callback:function(response,convo){
-              convo.repeat();
               convo.next();
+              convo.repeat();
+              //convo.next();
             }
           }
         ],{},'default');
+        */
+        ////convo.next();
       });
     }
     else{
@@ -104,20 +118,31 @@ controller.hears(['configure'],['direct_message','direct_mention','mention'],fun
           if(response.text.toLowerCase().includes("node")){
             //TODO:store environment in the database
             convo.say("Great, give me a minute to setup the configuration file! I'll let you know when it's done..");
-            convo.next();
+            //convo.next();
             initializeRepository(bot,message,repoName,"node");
 
           }
           else{
             convo.say("Sorry, I am not smart enough to create that config :disappointed:");
-            convo.next();
+            //convo.next();
           }
         });
+
       });
     }
     else{
       bot.reply(message,helpCommands().configure);
     }
+});
+//test last build and create issue on failure
+controller.hears(['test last build'],['direct_message','direct_mention','mention'],function(bot,message){
+
+  Travis.lastBuild(globals.ownerMap[message.channel],globals.repoMap[message.channel],function(data){
+    bot.reply(message,data.message);
+    if(data.status==='failure')
+      issueCreationConversation(bot,message,`Build failure`);
+  });
+
 });
 //testing issue creation
 controller.hears(['test issue'],['direct_message','direct_mention','mention'],function(bot,message){
@@ -128,7 +153,7 @@ controller.hears(['test travis','test Travis'],['direct_message','direct_mention
   bot.reply(message,"Testing travis");
 });
 
-//testing Coveralls
+//testing Coveralls and issue creation
 controller.hears(['test coveralls','test Coveralls'],['direct_message','direct_mention','mention'],function(bot,message){
 
   var coverage = Coveralls.getCoverageInfo("123",globals.coverageMap[message.channel]);
@@ -142,7 +167,7 @@ controller.hears(['test coveralls','test Coveralls'],['direct_message','direct_m
 
 });
 
-//testing Coveralls
+//setting Coveralls threshold
 controller.hears(['set coverage threshold','set threshold'],['direct_message','direct_mention','mention'],function(bot,message){
   //TODO add bounds between 0 - 100 as it is percentage
   var messageArray = message.text.split(' ');
@@ -158,6 +183,8 @@ controller.hears(['set coverage threshold','set threshold'],['direct_message','d
   }
 });
 
+//help section
+//TODO:test last build help
 controller.hears(['help'],['direct_message','direct_mention','mention'],function(bot,message){
   var messageArray = message.text.split(' ');
 
@@ -183,8 +210,8 @@ controller.hears(['help'],['direct_message','direct_mention','mention'],function
 
 function helpCommands(){
   return{
-    init:"*_init <repository>_*",
-    configure:"*_configure <repository>_*",
+    init:"*_init travis <owner>/<repo_name>_*",
+    configure:"*_configure <owner>/<repository>_*",
     issue:"*_test issue_*",
     travis:"*_test travis_*",
     coveralls:"*_test coveralls_*"
@@ -209,49 +236,63 @@ function initializeRepository(bot,message,repoName,framework){
 
 function issueCreationConversation(bot,message,issueTitle){
 
+  var issueFlag = true,issueChange = false;
+
   bot.startConversation(message,function(err,convo){
-    if(!issueTitle){
-      convo.ask('Please enter the title of the issue',function(response,convo){
-        if(response!==null && response.text!==""){
-            convo.setVar('issueName',response.text);
+
+    convo.addQuestion('Do you want to create an issue (yes/no)?',function(response,convo){
+      if(response.text.toLowerCase().includes("yes")){
+        issueFlag = true;
+      }
+      else{
+        issueFlag = false;
+      }
+      convo.next();
+    },{},'default');
+
+
+
+    if(issueFlag){
+      //if no issue title exists
+      if(!issueTitle || issueTitle===""){
+          convo.addQuestion('Please enter the title of the issue',function(response,convo){
+            convo.say("Thanks! I'll name the issue *"+response.text+"*");
+            issueTitle = response.text;
             convo.next();
-        }else{
-          convo.repeat();
+          },{},'default');
+      }
+      else{
+          convo.addQuestion('Current issue title is set to *_'+issueTitle+'_*.Do you want to change the title of the issue (yes/no)?',function(response,convo){
+            if(response.text.toLowerCase().includes("yes")){
+              issueChange = true;
+            }else{
+              issueChange = false;
+            }
+            convo.next();
+          },{},'default');
+
+      }
+
+      if(issueChange){
+          convo.addQuestion('Please enter the title of the issue',function(response,convo){
+            convo.say("Thanks! I'll name the issue *"+response.text+"*");
+            issueTitle = response.text;
+            convo.next();
+          },{},'default');
+      }else{
+        bot.reply(message,"No problem, I'll keep the title as it is!");
+      }
+
+        convo.addQuestion('Please enter a comma-separated list of assignees to the issue. Ex @user1,@user2,@user3...',function(response,convo){
+          var assigneeList = response.text.split(',');
+          var listOutput = response.text;
+          convo.say(`I am going to create an issue titled *${issueTitle}* and assign it to `+listOutput);
           convo.next();
-        }
-      });
+        },{},'default');
+
     }
     else{
-      convo.ask('Current issue title is set to *_'+issueTitle+'_*.Do you want to change the title of the issue (yes/no)?',function(response,convo){
-        if(response==='yes'){
-          convo.ask('Please enter the title of the issue',function(response,convo){
-            if(response!==null && response.text!==""){
-                convo.setVar('issueName',response.text);
-                convo.next();
-            }else{
-              convo.repeat();
-              convo.next();
-            }
-          });
-        }
-        else{
-          convo.setVar('issueName',issueTitle);
-          convo.next();
-        }
-      });
+      bot.reply(message,'Okay, I\'m not creating an issue');
     }
-    convo.ask('Please enter a comma-separated list of assignees to the issue. Ex @user1,@user2,@user3...',function(response,convo){
-      if(response!==null && response.text!==""){
-        var assigneeList = response.text.split(',');
-        convo.setVar('assigneeList',assigneeList);
-        convo.next();
-        var listOutput = response.text;
-        convo.say("I am going to create an issue titled *_{{vars.issueName}}_* and assign it to "+listOutput);
-        console.log(listOutput);
-      }else{
-        convo.repeat();
-        convo.next();
-      }
-    });
   });
 }
