8f015e30b82c0aa42381034549c80b61cac8756c
sssaha2 <sssaha2@ncsu.edu>
2017-11-08 19:12:18 -0500

    added localtunnel and express functionality:

--- node/bot.js
+++ node/bot.js
@@ -1,12 +1,31 @@
 
+var localtunnel  = require('localtunnel');
 var Botkit = require('botkit');
 var Coveralls = require('./modules/coveralls');
 var Travis = require('./modules/travis');
 var Github = require('./modules/github');
+var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var controller = Botkit.slackbot({
   debug: false
   //include "log: false" to disable logging
   //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+var tunnel = localtunnel(3000, function(err, tunnel) {
+    if (err){
+      console.log(error);
+    }// the assigned public url for your tunnel
+    // i.e. https://abcdefgjhij.localtunnel.me
+    console.log(tunnel.url);
+});
+
+tunnel.on('close', function() {
+    // tunnels are closed
 });
 
 var tempIssueName = "";
@@ -18,6 +37,13 @@ var globals = {
   yamlMap:{},//channel:true/false [check if yaml exisits or not]
   defaultThreshold:95,
 };
+//start the local webserver
+app.post("/",function(req,res){
+  console.log(req.body);
+  res.send("Hello world");
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 // connect the bot to a stream of messages
 controller.spawn({
@@ -28,7 +54,7 @@ controller.spawn({
 controller.hears(['init travis'],['direct_message','direct_mention','mention'],function(bot,message){
   var messageArray = message.text.split(' ');
   var index = messageArray.indexOf('travis');
-
+  console.log(tunnel.url);
   if(messageArray.indexOf('help')===-1 && messageArray.indexOf('travis')!==-1 && messageArray.indexOf('init')!==-1){
     //repo name has to be word after init
     var repoString = null;
@@ -187,7 +213,6 @@ controller.hears(['test coveralls','test Coveralls'],['direct_message','direct_m
        var coverageBelowThreshold = globals.coverageMap[message.channel] - coverage.data.body.covered_percent;
        issueCreationConversation(bot,message,`Coverage ${coverageBelowThreshold} percent below threshold`);
       }
-
     });
   }
 });
