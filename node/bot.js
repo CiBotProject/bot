c584c99dae45da8bd84488b26c6d65e00ecaba4e
arewm <ajmcnama@ncsu.edu>
2017-11-30 15:10:23 -0500

    integrate .yaml deletion on reset

--- node/bot.js
+++ node/bot.js
@@ -125,19 +125,12 @@ controller.hears(['add-token'], ['direct_message'], function (bot, message) {
   tokenManager.addToken(messageArray[0], messageArray[1]);
   bot.reply(message, `The user "${messageArray[0]}" token "${messageArray[1]}" is stored:tada::tada::tada:.`)
 });
+
 // reset repository
 controller.hears(['reset travis'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-  getChannelDataOrPromptForInit(message, 'reset travis', function(channel_data){
-      controller.storage.channels.delete(message.channel, function (err) {
-        console.log(err)
-        if (err) {
-          bot.reply(message, `There was an error resetting this channel: ${err}`);
-          return;
-        }
-      });
-      bot.reply(message, `The channel has been reset. It was initialized to ${channel_data.owner}/${channel_data.repo}`)
-  })
+  bot.startConversation(message, askChannelReset);
 });
+
 //init repository
 controller.hears(['init travis'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
 
@@ -176,14 +169,6 @@ controller.hears(['init travis'], ['direct_message', 'direct_mention', 'mention'
               'body': ''
             }
           });
-          // //map channel to repo
-          // slack_data.set(`${message.channel}.repo`,repoContent[1]);
-          // //map repo to channel
-          // slack_data.set(`${repoContent[1]}.channel`,message.channel);
-          // //map channel to owner
-          // slack_data.set(`${message.channel}.owner`,repoContent[0]);
-          // //create default coverageMap entry
-          // slack_data.set(`${message.channel}.coverage`,slack_data.get("defaultThreshold"));
 
           //console.log(tokenManager.getToken())
           if (tokenManager.getToken(repoContent[0]) === null) {
@@ -191,13 +176,15 @@ controller.hears(['init travis'], ['direct_message', 'direct_mention', 'mention'
             return;
           }
 
-          Travis.activate(repoContent[0], repoContent[1], function (data) {
-            bot.reply(message, data.message);
-            if (data.status === 'error')
-              return;
-            bot.startConversation(message, askYamlCreation);
+          // Enable issues before activating Travis
+          Github.enableIssues(repoContent[0], repoContent[1]).then(function () {
+            Travis.activate(repoContent[0], repoContent[1], function (data) {
+              bot.reply(message, data.message);
+              if (data.status === 'error')
+                return;
+              bot.startConversation(message, askYamlCreation);
+            });
           });
-
         }
         else {
           bot.reply(message, "Please provide the name of the repository to be initialized. Ex init travis <owner>/<repository>");
@@ -212,10 +199,10 @@ controller.hears(['init travis'], ['direct_message', 'direct_mention', 'mention'
 });
 
 //helper functions
-function getChannelDataOrPromptForInit (message, location, callback){
+function getChannelDataOrPromptForInit(message, location, callback) {
   console.log(message)
   console.log(location)
-  controller.storage.channels.get(message.channel, function(err, channel_data){
+  controller.storage.channels.get(message.channel, function (err, channel_data) {
     if (callback === undefined) {
       callback = location
       location = '';
@@ -223,7 +210,7 @@ function getChannelDataOrPromptForInit (message, location, callback){
     else {
       location += ' ';
     }
-    if (channel_data){
+    if (channel_data) {
       callback(channel_data);
     }
     else {
@@ -233,14 +220,14 @@ function getChannelDataOrPromptForInit (message, location, callback){
   })
 }
 
-function saveChannelDataLogError (data, location) {
+function saveChannelDataLogError(data, location) {
   if (location === undefined) {
     location = '';
   }
   else {
     location += ' ';
   }
-  controller.storage.channels.save(data, function(err){
+  controller.storage.channels.save(data, function (err) {
     if (err) {
       console.log(`Data save ${location}failed.\ndata: ${JSON.stringify(data)}\nerror: ${err}`)
     }
@@ -266,12 +253,45 @@ askYamlCreation = function (response, convo) {
   });
 }
 
-askLanguageToUse = function (response, convo) {
+function askChannelReset(response, convo){
+  convo.ask('Would you like to delete the yaml file from the repo (yes/no)?', function (response, convo) {
+    getChannelDataOrPromptForInit(convo.source_message, 'askChannelReset', function (channel_data) {
+      if (response.text.toLowerCase() === "yes") {
+        Github.deleteFile(channel_data.owner, channel_data.repo, '.travis.yml').then(function (res) {
+          convo.say(`Yaml file successfully deleted.`);
+          convo.next();
+        }, function (res) {
+          convo.say(`Error deleting the yaml file.`);
+          convo.next();
+        })
+      } else if (response.text.toLowerCase() === "no") {
+        convo.say("The yaml file has been left alone.");
+        convo.next();
+      }
+      else {
+        convo.say("I consider this response to be a 'no'. I have left the yaml file.");
+        convo.next();
+      }
+      controller.storage.channels.delete(channel_data.id, function (err) {
+        console.log(err)
+        if (err) {
+          convo.say(`There was an error resetting this channel: ${err}`);
+          convo.next();
+          return;
+        }
+      });
+      convo.say(`The channel has been reset. It was initialized to ${channel_data.owner}/${channel_data.repo}`);
+    });
+    convo.next();
+  })
+}
+
+function askLanguageToUse (response, convo) {
   convo.ask('Which language do you want to use? ' + Travis.listTechnologies().data.body.join(', '), function (response, convo) {
-    getChannelDataOrPromptForInit(response, 'askLanguageToUse', function(channel_data){
+    getChannelDataOrPromptForInit(convo.source_message, 'askLanguageToUse', function (channel_data) {
       let repo = channel_data.repo;
       let owner = channel_data.owner;
-      var yamlStatus = Travis.createYaml(response.text, myUrl, response.channel);
+      var yamlStatus = Travis.createYaml(response.text, myUrl, channel_data.id);
       if (yamlStatus.status === 'success') {
         //yamlStatus.data.body needs to be passed
         convo.say("I am pushing the yaml file to the github repository ");
@@ -282,9 +302,9 @@ askLanguageToUse = function (response, convo) {
         }).catch(function (res) {
 
           convo.say("Error pushing the yaml file to the github repository. Please try and run init travis <owner>/<reponame> ensuring correct details ");
-          controller.storage.channels.delete(response.channel, function (err) {
+          controller.storage.channels.delete(channel_data.id, function (err) {
             if (err)
-              console.log(`Tried resetting channel ${response.channel}; received error ${err}`)
+              console.log(`Tried resetting channel ${channel_data.id}; received error ${err}`)
           })
           // slack_data.delete(repo);
 
@@ -293,9 +313,9 @@ askLanguageToUse = function (response, convo) {
       else {
         convo.say("Error in creating yaml file");
         convo.say("See https://docs.travis-ci.com/user/languages/ to set up your repository.");
-        controller.storage.channels.delete(response.channel, function (err) {
+        controller.storage.channels.delete(channel_data.id, function (err) {
           if (err)
-            console.log(`Tried resetting channel ${response.channel}; received error ${err}`)
+            console.log(`Tried resetting channel ${channel_data.id}; received error ${err}`)
         })
         // slack_data.delete(repo);
       }
@@ -342,12 +362,10 @@ controller.hears(['configure yaml'], ['direct_message', 'direct_mention', 'menti
 //testing issue creation
 controller.hears(['create issue'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
   console.log(message)
-  getChannelDataOrPromptForInit(message, 'create issue', function(channel_data){
+  getChannelDataOrPromptForInit(message, 'create issue', function (channel_data) {
     bot.startConversation(message, askToCreateIssue);
   })
 });
-
-
 
 //setting Coveralls threshold
 controller.hears(['set coverage threshold', 'set threshold'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
@@ -356,7 +374,7 @@ controller.hears(['set coverage threshold', 'set threshold'], ['direct_message',
   var index = messageArray.indexOf('to');
 
   //repo name has to be word after init
-  getChannelDataOrPromptForInit(message, 'set threshold', function(channel_data){
+  getChannelDataOrPromptForInit(message, 'set threshold', function (channel_data) {
     if ((index + 1) < messageArray.length) {
       channel_data.coverage = parseInt(messageArray[index + 1]);
       controller.storage.channels.save(message.channel, channel_data, function (err) {
@@ -418,7 +436,7 @@ function initializeRepository(bot, message, repoName, framework) {
 //helper functions
 
 askToCreateIssue = function (response, convo) {
-  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateIssue', function(channel_data){
+  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateIssue', function (channel_data) {
     if (channel_data.issue.title === "") {
       askToCreateNewIssue(response, convo);
     }
@@ -429,7 +447,7 @@ askToCreateIssue = function (response, convo) {
 }
 
 askToCreateNewIssue = function (response, convo) {
-  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateNewIssue', function(channel_data){
+  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateNewIssue', function (channel_data) {
     convo.ask('Please enter the name of the issue', function (response, convo) {
       channel_data.issue.title = response.text;
       saveChannelDataLogError(channel_data, 'askToCreateNewIssue')
@@ -442,7 +460,7 @@ askToCreateNewIssue = function (response, convo) {
 }
 
 askToCreateExistingIssue = function (response, convo) {
-  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateExistingIssue', function(channel_data){
+  getChannelDataOrPromptForInit(convo.source_message, 'askToCreateExistingIssue', function (channel_data) {
     let name = channel_data.issue.title;
     let body = '';
 
@@ -468,17 +486,17 @@ askToCreateExistingIssue = function (response, convo) {
 }
 
 askToAssignPeople = function (response, convo) {
-  getChannelDataOrPromptForInit(convo.source_message, 'askToAssignPeople', function(channel_data){
+  getChannelDataOrPromptForInit(convo.source_message, 'askToAssignPeople', function (channel_data) {
     convo.ask('Please enter a comma-separated list of github usernames to the issue. Ex user1,user2,user3...', function (response, convo) {
       let listOutput = response.text;
       console.log(response.text);
-  
+
       // split and strip assignees
-      let listOfassignees = listOutput.split(",").map(function(item) {
+      let listOfassignees = listOutput.split(",").map(function (item) {
         return item.trim();
       });;
       let issueName = channel_data.issue.title
-  
+
       convo.say(`I am going to create an issue titled *${issueName}* and assign it to ` + listOutput);
 
       let repo = channel_data.repo;
